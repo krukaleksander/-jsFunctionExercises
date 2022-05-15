2458ae4adaa01a0fe4d29c1a6e1cae531e36c827
aleksander <kruk.aleksander@hotmail.com>
2022-05-15 08:50:02 +0200

    feat: function that flattens a list of items

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import {say, sumOfArray} from "./index";
+import {flat, say, sumOfArray} from "./index";
 
 describe('usage of closures', function () {
     it('should greet user', function () {
@@ -11,5 +11,13 @@ describe('usage of closures', function () {
 describe('sum of array', () => {
     it('should return sum of array elements', function () {
         expect(sumOfArray([9, 1, 22, 0, 2])).toBe(34)
+    });
+})
+
+describe('flat an array', () => {
+    it('should flat nested arrays', function () {
+        const exampleArray = [[2, [4, [44,5,6]]], [4,5,6], [[2,4], 4], 5];
+        const exampleOutput = [2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5];
+expect(flat(exampleArray)).toEqual(exampleOutput)
     });
 })
--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ export const sumOfArray = (arr: number[]) => arr.reduce((previousValue, currentV
 // 3. Please write a recursive function that flattens a list of items
 // example input [[2, [4, [44,5,6]]], [4,5,6], [[2,4], 4], 5]]
 // example output [2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5]
+export const flat = exampleArray => exampleArray.reduce((previousValue, currentValue) => !Array.isArray(currentValue) ? previousValue.concat(currentValue)  : previousValue.concat(flat(currentValue)), []);
 
 // 4. Please write a function that finds all common elements of two arrays(only primitive types as array elements, order doesn't matter)
 // example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
